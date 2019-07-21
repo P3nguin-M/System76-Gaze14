f135a7f85ad8cedc43448205d923863beea8d921
P3nguin-M <38765520+P3nguin-M@users.noreply.github.com>
2019-07-21 13:36:41 -0500

    Add files via upload

---
+++ enable-touch-type.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## authored by t0x1cbyt3 on Ubuntu 18.04.02 LTS
+##
+
+## Parse device value of Touchpad Input device
+device=$(xinput| grep 06CB | awk -F 'Touchpad' '{print $2}'| cut -f 2)
+device=${device:3}
+
+## Parse property value of "Disable while Typing"
+prop=$(xinput list-props $device| grep "Disable While Typ"| head -n 1| awk -F '(' '{print $2}')
+prop=${prop:0:3}
+
+##
+## Check if any input is given
+## and if not, set to default 0
+##
+if [ $# -eq 1 ]
+  then
+		xinput --set-prop $device $prop $1
+  else
+  		echo 'Add a "1"  as an argument to enable "Disable Touchpad While Typing"'
+		xinput --set-prop $device $prop 0
+fi
+exit 0
